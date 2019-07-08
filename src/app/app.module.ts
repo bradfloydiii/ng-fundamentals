333f82b9d5d596456b64b2492b017070f4546f03
bradfloydiii <bfloydiii@gmail.com>
2019-07-08 11:27:21 -0400

    component communication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EventsComponent } from './events/events.component';
+import { EventsListComponent } from './events-list/events-list.component';
+import { EventThumbnailComponent } from './event-thumbnail/event-thumbnail.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
