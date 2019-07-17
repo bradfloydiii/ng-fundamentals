5003c2790fa26ace6e5596b1b28b012e8a874ae2
bradfloydiii <bfloydiii@gmail.com>
2019-07-16 21:25:22 -0400

    colored hearts

--- src/app/events/event-details/upvote/upvote.component.ts
+++ src/app/events/event-details/upvote/upvote.component.ts
@@ -7,8 +7,11 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class UpvoteComponent {
   @Input() count: any;
-  @Input() voted: any;
+  @Input() set voted(val) {
+    this.iconColor = val ? 'red' : 'white';
+  }
   @Output() vote = new EventEmitter();
+  iconColor: string;
 
   constructor() {}
 
